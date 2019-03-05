9ce569b57b93ce4b69da49fb5374a2c8f1f82491
David Tanderup <Tanderud@my.erau.edu>
2019-03-05 10:17:29 -0500

    Add project files.

---
+++ LongList/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using LongListLibrary;
+
+namespace LongList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                           
+                      
+                        
+            
+            // Creation of the master list.
+            List<Tuple<string, bool>> MyList = new List<Tuple<string, bool>>();
+     
+
+            // The call to add new things to the list.
+            MyList.Add(new Tuple<string, bool>("Jim",true));
+
+ 
+            // unpack a Tuple
+            var myTuple = MyList[0];
+            var row = myTuple.Item1;  // value of 0
+            var col = myTuple.Item2;  // value of 1
+
+            Console.WriteLine(row);
+
+
+
+            MyList.Insert(0, new Tuple<string, bool>( "cat", true ));
+            
+            // Remove items from the list
+            MyList.RemoveRange(0,3);
+
+
+            // Ref: stackoverflow.com/questions/20823698/c-sharp-serialize-deserialize-or-save-load-a-list-of-tuples
+            
+            FileStream stream = new FileStream(@"C:\Users\David Tanderup\Desktop\Data2.dat", FileMode.Create);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, MyList);
+            stream.Close();
+            
+            Console.WriteLine("Load filename.dat...");
+            FileStream inStr = new FileStream(@"C:\Users\David Tanderup\Desktop\Data2.dat", FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();
+            MyList = bf.Deserialize(inStr) as List<Tuple<string, bool>>;
+            Console.WriteLine($"{MyList[0]} {MyList.Count()}" );
+            
+            //File.Open(@"C:\Users\David Tanderup\Desktop\Data.txt",FileMode.OpenOrCreate).Close();
+
+            //File.(@"C:\Users\David Tanderup\Desktop\Data.txt", MyList);
+            
+            Console.ReadLine();
+        }
+    }
+    public class BigLongList
+    {
+
+
+
+    }
+}
---
+++ LongListLibrary/Class1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongListLibrary
+{
+    public class Class1
+    {
+    }
+
+    public class List
+    {
+
+        List<Tuple<string, bool>> MyList = new List<Tuple<string, bool>>();
+
+        /// <summary>
+        /// Adds a new item to the list and then updates the Data File
+        /// t.2 is set to false indicating the item has not been completed.
+        /// </summary>
+        
+        public void AddThingsToMyList(string userInput)
+        {
+            
+            Files files = new Files();
+
+            Console.WriteLine("");
+            
+            userInput = Console.ReadLine();
+
+            MyList.Add(new Tuple<string, bool>(userInput, false));
+
+            files.SaveItem(MyList);
+
+        }
+       
+        /// <summary>
+        /// Removes the items from the specified location on the list.
+        /// </summary>
+
+        public void RemoveThingsFromMyList(List<Tuple<string,bool>> MyList, int indexNumber)
+        {
+            MyList.RemoveRange(0, indexNumber);
+
+        }
+
+
+
+
+    }
+
+
+
+
+    public class Files
+    {
+        // Ref: stackoverflow.com/questions/20823698/c-sharp-serialize-deserialize-or-save-
+        //      load-a-list-of-tuples
+
+
+
+
+        /// <summary>
+        /// Saves the changes made to the data file
+        /// </summary>
+
+        public void SaveItem(List<Tuple<string,bool>> MyList)
+        {
+
+            FileStream stream = new FileStream(@"C:\Users\David Tanderup\Desktop\Data2.dat", FileMode.Create);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            formatter.Serialize(stream, MyList);
+
+            stream.Close();
+        }
+
+
+    }
+
+
+
+
+
+}
