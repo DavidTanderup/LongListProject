4298dca2c862c96fe5535e1646e109eb7e02f940
David Tanderup <Tanderud@my.erau.edu>
2019-03-07 10:26:25 -0500

    Everything is this working need to add select item & change status

--- LongList/Program.cs
+++ LongList/Program.cs
@@ -13,57 +13,125 @@ namespace LongList
     {
         static void Main(string[] args)
         {
-                           
-                      
-                        
+
             
-            // Creation of the master list.
-            List<Tuple<string, bool>> MyList = new List<Tuple<string, bool>>();
-     
+            //Files files = new Files();
 
-            // The call to add new things to the list.
-            MyList.Add(new Tuple<string, bool>("Jim",true));
+            //List<Tuple<string, bool>> MyList = files.LoadItem();
 
- 
-            // unpack a Tuple
-            var myTuple = MyList[0];
-            var row = myTuple.Item1;  // value of 0
-            var col = myTuple.Item2;  // value of 1
-
-            Console.WriteLine(row);
+            //foreach (Tuple<string, bool> item in MyList)
+            //{
+            //    Console.WriteLine($"{item.Item1}");
+            //}
 
 
+            MainMenu mM = new MainMenu();
+            bool displayMenu = true;
+            while (displayMenu)
+            {
+                mM.WelcomeToYourList();
+                displayMenu = mM.MainMenuUserInput();
 
-            MyList.Insert(0, new Tuple<string, bool>( "cat", true ));
+            }
+
+
+            // Add Input selection method
+
+
+            //ClassLists cL = new ClassLists();
+            //Files files = new Files();
+            //ProgramLaunch pL = new ProgramLaunch();
+
+
+            //foreach (Tuple<string, bool> item in MyList)
+            //{
+            //    Console.WriteLine($"{item.Item1}");
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
             
-            // Remove items from the list
-            MyList.RemoveRange(0,3);
+            Console.Write("Enter Item: ");
+            string userInput = Console.ReadLine();
+
+            cL.AddThingsToMyList(userInput);
+
+                                                     
+            //List<Tuple<string, bool>> MyList = files.LoadItem();
+            MyList.Add(new Tuple<string, bool>("Jim",false));
+            files.SaveItem(MyList);
 
 
-            // Ref: stackoverflow.com/questions/20823698/c-sharp-serialize-deserialize-or-save-load-a-list-of-tuples
+            int index = 1;
+            int count;
+            if (MyList.Count() > 25)
+            {
+                count = 25;
+            }
+            else
+            {
+                count = MyList.Count();
+            }
+            int i = 0;
+            while (count > 0)
+            {
+                var tuple = MyList[i];
+                string x = tuple.Item1;
+                bool y = tuple.Item2;
+                if (y != true)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"\n\t{index} {tuple.Item1}");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine($"\n\t{index} {tuple.Item1}");
+                }
+                count--;
+                index++;
+                i++;
+            }
             
-            FileStream stream = new FileStream(@"C:\Users\David Tanderup\Desktop\Data2.dat", FileMode.Create);
-            BinaryFormatter formatter = new BinaryFormatter();
-            formatter.Serialize(stream, MyList);
-            stream.Close();
             
-            Console.WriteLine("Load filename.dat...");
-            FileStream inStr = new FileStream(@"C:\Users\David Tanderup\Desktop\Data2.dat", FileMode.Open);
-            BinaryFormatter bf = new BinaryFormatter();
-            MyList = bf.Deserialize(inStr) as List<Tuple<string, bool>>;
-            Console.WriteLine($"{MyList[0]} {MyList.Count()}" );
-            
-            //File.Open(@"C:\Users\David Tanderup\Desktop\Data.txt",FileMode.OpenOrCreate).Close();
 
-            //File.(@"C:\Users\David Tanderup\Desktop\Data.txt", MyList);
-            
+
+                */
+            Console.Clear();
+            Console.WriteLine("Press any key to exit....");
             Console.ReadLine();
+
+
+
+
+
+
+
+
         }
+
+
+
+
+
+
+
     }
-    public class BigLongList
-    {
 
 
 
-    }
+
+
+
+
 }
--- LongListLibrary/Class1.cs
+++ LongListLibrary/Class1.cs
@@ -8,39 +8,31 @@ using System.Threading.Tasks;
 
 namespace LongListLibrary
 {
-    public class Class1
-    {
-    }
-
-    public class List
+    public class ClassLists
     {
 
-        List<Tuple<string, bool>> MyList = new List<Tuple<string, bool>>();
+        
 
         /// <summary>
         /// Adds a new item to the list and then updates the Data File
         /// t.2 is set to false indicating the item has not been completed.
         /// </summary>
-        
-        public void AddThingsToMyList(string userInput)
+        public void AddThingsToMyList(string userInput, List<Tuple<string, bool>> MyList)
         {
             
             Files files = new Files();
-
-            Console.WriteLine("");
             
-            userInput = Console.ReadLine();
-
+            
             MyList.Add(new Tuple<string, bool>(userInput, false));
 
             files.SaveItem(MyList);
 
         }
+        
        
         /// <summary>
         /// Removes the items from the specified location on the list.
         /// </summary>
-
         public void RemoveThingsFromMyList(List<Tuple<string,bool>> MyList, int indexNumber)
         {
             MyList.RemoveRange(0, indexNumber);
@@ -48,12 +40,34 @@ namespace LongListLibrary
         }
 
 
+        /// <summary>
+        /// Marks the item in the list complete by creating a tuple with the altered bool value
+        /// in the same position as the previous version. The list is then saved 
+        /// to the data file.
+        /// </summary>
+        public void MarkedAsComplete(List<Tuple<string, bool>> MyList, int indexNumber)
+        {
+            Files files = new Files();
+
+
+
+            // Unpack the tuple
+            var myTuple = MyList[indexNumber];
+            var row = myTuple.Item1;  // value of 0
+            var col = myTuple.Item2;  // value of 1
+
+            // creates a new tuple in the index position of the old value
+            MyList[indexNumber] = new Tuple<string, bool>(row, true);
+
+            files.SaveItem(MyList);
+
+      
+        }
+
+
 
 
     }
-
-
-
 
     public class Files
     {
@@ -61,16 +75,13 @@ namespace LongListLibrary
         //      load-a-list-of-tuples
 
 
-
-
         /// <summary>
         /// Saves the changes made to the data file
         /// </summary>
-
         public void SaveItem(List<Tuple<string,bool>> MyList)
         {
 
-            FileStream stream = new FileStream(@"C:\Users\David Tanderup\Desktop\Data2.dat", FileMode.Create);
+            FileStream stream = new FileStream(@"C:\Users\WWStudent\Desktop\Data2.dat", FileMode.OpenOrCreate);
 
             BinaryFormatter formatter = new BinaryFormatter();
 
@@ -80,7 +91,199 @@ namespace LongListLibrary
         }
 
 
+        /// <summary>
+        /// Loads populates program list from the data save file
+        /// </summary>
+        public List<Tuple<string, bool>> LoadItem()
+        {
+            List<Tuple<string, bool>> MyList = new List<Tuple<string, bool>>();
+
+            FileStream inStr = new FileStream(@"C:\Users\WWStudent\Desktop\Data2.dat", FileMode.Open);
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            MyList = bf.Deserialize(inStr) as List<Tuple<string, bool>>;
+
+            inStr.Close();
+                       
+            return MyList;
+        }
+
+
+        
+
     }
+
+
+
+    public class ProgramLaunch
+    {
+        public void Formalities ()
+        {
+            Files files = new Files();
+            files.LoadItem();
+        }
+
+        /// <summary>
+        /// reviews the first 25 items to see if they have been completed. If they have been 
+        /// completed they are deleted.
+        /// </summary>
+
+        
+
+    }
+
+
+    public class MainMenu
+    {
+        /// <summary>
+        /// Clears the screen and sets white as default color
+        /// </summary>
+        public void MenuDisplaySettings()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// Displays the text for the entry menu.
+        /// </summary>
+        public void WelcomeToYourList()
+        {
+            MenuDisplaySettings();
+
+            Console.WriteLine("\tTo-Do List Main Menu");
+
+            Console.WriteLine("-------------------------------------");
+
+            Console.WriteLine("\t1) Add Tasks");
+
+            Console.WriteLine("\n\t2) View Current List");
+
+            Console.WriteLine("\n\t3) Quit");
+
+            Console.Write("\nPlease Make Your Selection: ");
+        }
+
+        /// <summary>
+        /// Takes the user's input and redirects to the correct method.
+        /// </summary>
+        /// <returns></returns>       
+        public bool MainMenuUserInput()
+        {   
+            
+            var userInput = Console.ReadLine();
+            // Add Tasks to the list
+            if (userInput == "1")
+            {
+                AddToListMenu();
+                return true;
+            }
+            // View Task List
+            else if (userInput == "2")
+            {
+                ViewListMenu();
+                return true;
+
+            }
+            // Quit
+            else if (userInput == "3")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+                                               
+        /// <summary>
+        /// Adding Items to the user list by importing the list from file. Adding to the list and then saving the list.
+        /// After enter the item the user is returned to the main menu
+        /// </summary>
+        public void AddToListMenu()
+        {
+            MenuDisplaySettings();
+
+            ClassLists cL = new ClassLists();
+
+            Files files = new Files();
+
+            List<Tuple<string, bool>> MyList = files.LoadItem();
+
+            Console.WriteLine("\tAdd Task Menu");
+            
+            // Change text
+            Console.Write("Add Something: ");
+
+            string userInput = Console.ReadLine();
+
+            cL.AddThingsToMyList(userInput, MyList);
+
+        }
+
+
+
+
+
+
+        /// <summary>
+        /// View menu pulls from the load method > which opens the file location and assigns the list values to "MyList"
+        /// </summary>
+        public  void ViewListMenu()
+        {
+            MenuDisplaySettings();
+            Files files = new Files();
+
+            List<Tuple<string, bool>> MyList = files.LoadItem();
+
+            Console.WriteLine("\tTo-Do List");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Complete");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("------------------");
+
+
+            int i = 1;
+            int index = MyList.Count();
+
+            foreach (Tuple<string, bool> item in MyList.GetRange(0,index))
+            {
+                if (item.Item2 == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine($"{i++}){item.Item1}");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"{i++}) {item.Item1}");
+                }
+            }
+
+            /// Select item
+            
+            ///     a.Mark as Complete
+            ///     b.Mark as Incomplete 
+            ///         
+            /// Next page (if more than 25)
+            /// Previous page
+
+
+
+
+            Console.ReadLine();
+
+        }
+
+
+
+
+
+    }
+
+
 
 
 
